60294b5b84934ed71499ad1e52ac513a024493be
anilejder <anilejder@gmail.com>
2020-04-17 20:08:35 +0300

    Initial commit

---
+++ app/src/main/java/com/tt/weatherplanner/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tt.weatherplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
